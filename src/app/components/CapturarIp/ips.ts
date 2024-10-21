f9fac3775d8ead1dd750c3af23852f93678218e7
Alan934 <sanjurjoalan77@gmail.com>
2024-10-21 19:29:40 -0300

    update ip

--- src/app/components/CapturarIp/ips.ts
+++ src/app/components/CapturarIp/ips.ts
@@ -9,13 +9,14 @@ export default function Home({ ip }: { ip: string }) {
     const sendIpToBackend = async () => {
       try {
         // Enviar la IP capturada al backend
-        await postIp({ ip }).unwrap();
-        console.log("IP enviada:", ip);
+        console.log("Enviando IP al backend:", ip);
+        const response = await postIp({ ip }).unwrap();
+        console.log("Respuesta del backend:", response);
       } catch (error) {
         console.error("Error al enviar la IP:", error);
       }
     };
-
+  
     sendIpToBackend(); // Enviar la IP al cargar la página
   }, [ip, postIp]);
   console.log("IP enviada"+ ip);
@@ -24,7 +25,7 @@ export default function Home({ ip }: { ip: string }) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
-
+  console.log("IP obtenida en el servidor:", ip);
   return {
     props: {
       ip: ip || "IP desconocida",
